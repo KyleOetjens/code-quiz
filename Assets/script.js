40f3d2edea56809608d379e9561d3a1d8dbf79bf
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-01-07 14:46:59 -0600

    commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 let startGame = document.querySelector("#btn");
 let timeL = document.querySelector("#timer");
-let timeLeft = 15;
+let timeLeft = 30;
 let body = document.body
 let qdiv = document.createElement("div")
 let qh = document.createElement("h1")
@@ -11,17 +11,18 @@ let incorrectAnswer2;
 let incorrectAnswer3;
 let timeInterval;
 let zScore = localStorage.getItem("points");
-    let zName = localStorage.getItem("name")
-    document.querySelector("#score").innerHTML = zScore;
-    document.querySelector("#name").innerHTML = zName
-
+let zName = localStorage.getItem("name")
+document.querySelector("#score").innerHTML = zScore;
+document.querySelector("#name").innerHTML = zName
 let score = {
-    initials: [],
     timeScore: [],
+    playerName: [],
     questionsRight: []
 }
+console.log(score);
+//functions that run throughout the game
 function addTime() {
-    timeLeft = timeLeft + 25
+    timeLeft = timeLeft + 5
 }
 function subtractTime() {
     timeLeft = timeLeft - 5
@@ -43,10 +44,12 @@ function savePoints() {
     localStorage.setItem("points", score.timeScore)
     localStorage.setItem("questionsRight", score.questionsRight)
 }
+//event listeners to start the quiz
 startGame.addEventListener("click", startTime)
-
+startGame.addEventListener("click", showQuestions)
+//function to run the countdown
 function startTime() {
-    timeLeft = 15;
+    timeLeft = 30;
     timeL.textContent = timeLeft + " seconds left";
     timeInterval = setInterval(function () {
         timeLeft--;
@@ -61,25 +64,23 @@ function startTime() {
 function sendMessage() {
     timeL.textContent = "Game Over"
 }
-
-startGame.addEventListener("click", showQuestions)
-
+// group of functions for questions
 function showQuestions(event) {
     console.log(score);
+    console.log(score);
     console.log(event);
-    console.log("questions started");
     let b1 = document.createElement('button')
     let b2 = document.createElement('button')
     let b3 = document.createElement('button')
     let b4 = document.createElement('button')
     buildQuestions(b1, b2, b3, b4)
-    qh.textContent = "First Question"
-    b1.textContent = "Pizza"
-    b2.textContent = "Burgers"
-    b3.textContent = "Steak"
-    b4.textContent = "Fish"
-    let correctAnswer = b1;
-    let incorrectAnswer1 = b2;
+    qh.textContent = "First Question:  Which HTML tag links our JavaScript code?"
+    b1.textContent = "<js>"
+    b2.textContent = "<script>"
+    b3.textContent = "<javascript>"
+    b4.textContent = "<scripting>"
+    let correctAnswer = b2;
+    let incorrectAnswer1 = b1;
     let incorrectAnswer2 = b3;
     let incorrectAnswer3 = b4;
     correctAnswer.addEventListener("click", secondQuestion);
@@ -88,11 +89,11 @@ function showQuestions(event) {
     incorrectAnswer3.addEventListener("click", subtractTime);
 }
 function secondQuestion(event) {
+    console.log(score);
+    addTime();
     addScore();
     savePoints()
-    addTime();
     console.log(score);
-    console.log("2nd question started");
     let b1 = document.createElement('button')
     let b2 = document.createElement('button')
     let b3 = document.createElement('button')
@@ -102,33 +103,32 @@ function secondQuestion(event) {
     let q2incorrectAnswer2 = b2
     let q2incorrectAnswer3 = b4
     buildQuestions(b1, b2, b3, b4)
-    qh.textContent = "second Question"
-    b1.textContent = "snake"
-    b2.textContent = "bear"
-    b3.textContent = "dog"
-    b4.textContent = "bird"
+    qh.textContent = "Second Question: What is the correct syntax to link an external script?"
+    b1.textContent = `<script href="xxx.js">`
+    b2.textContent = `<script name="xxx.js">`
+    b3.textContent = `<script src="xxx.js">`
+    b4.textContent = `<script link="xxx.js">`
     q2correctAnswer.addEventListener("click", thirdQuestion);
     q2incorrectAnswer1.addEventListener("click", subtractTime);
     q2incorrectAnswer2.addEventListener("click", subtractTime);
     q2incorrectAnswer3.addEventListener("click", subtractTime);
 }
 function thirdQuestion(event) {
-    addScore()
-    savePoints()
     console.log(score);
     addTime();
-    console.log("3rd question started");
+    addScore();
+    savePoints()
     let b1 = document.createElement('button')
     let b2 = document.createElement('button')
     let b3 = document.createElement('button')
     let b4 = document.createElement('button')
     buildQuestions(b1, b2, b3, b4)
     let correctAnswer = b3
-    qh.textContent = "Third Question"
-    b1.textContent = "pop"
-    b2.textContent = "water"
-    b3.textContent = "milk"
-    b4.textContent = "juice"
+    qh.textContent = "Third Question: How do you call a function named runFunction()?"
+    b1.textContent = `function = runFunction()`
+    b2.textContent = `RunFunction()`
+    b3.textContent = `runFunction()`
+    b4.textContent = `var = runfunction()`
     let q3correctAnswer = b3
     let q3incorrectAnswer1 = b1
     let q3incorrectAnswer2 = b2
@@ -139,65 +139,40 @@ function thirdQuestion(event) {
     q3incorrectAnswer3.addEventListener('click', subtractTime)
 }
 function forthQuestion(event) {
-    addScore()
-    savePoints()
     console.log(score);
     addTime();
-    console.log("4th question started");
+    addScore();
+    savePoints()
     let b1 = document.createElement('button')
     let b2 = document.createElement('button')
     let b3 = document.createElement('button')
     let b4 = document.createElement('button')
     buildQuestions(b1, b2, b3, b4)
     let correctAnswer = b4;
-    qh.textContent = "last Question"
-    b1.textContent = "pop"
-    b2.textContent = "water"
-    b3.textContent = "milk"
-    b4.textContent = "juice"
+    qh.textContent = "Last Question: How does a for loop start?"
+    b1.textContent = `for (i<=5; i=0; i++)`
+    b2.textContent = `for (i++; i<=5; i=0`
+    b3.textContent = `for (i<=5; i++)`
+    b4.textContent = `for (i=0; i<=5; i++)`
     let q4correctAnswer = b4
     let q4incorrectAnswer1 = b1
     let q4incorrectAnswer2 = b2
     let q4incorrectAnswer3 = b3
-   // q4correctAnswer.addEventListener("click", addTime)
-    //q4correctAnswer.addEventListener("click", addTime;setScore1)
-    q4correctAnswer.addEventListener("click", function endGame()
-    {
-        addScore()
-        setScore1()
-    })
+    q4correctAnswer.addEventListener("click", addTime)
+    q4correctAnswer.addEventListener("click", addScore)
+    q4correctAnswer.addEventListener("click", setScore1)
     q4incorrectAnswer1.addEventListener('click', subtractTime)
     q4incorrectAnswer2.addEventListener('click', subtractTime)
     q4incorrectAnswer3.addEventListener('click', subtractTime)
 }
+//function to save name and score/ show in scoreboard at the end of the quiz
 function setScore1() {
     clearInterval(timeInterval);
     let scoreList1 = document.createElement("li")
     let scoreSpan = document.createElement("span")
     document.getElementById("scoreList").appendChild(scoreList1)
-    scoreList1.appendChild(scoreSpan)
     JSON.parse(localStorage.getItem("points"))
     let name1 = window.prompt("What is your name?")
     localStorage.setItem("name", name1)
-    scoreSpan.innerHTML = "Name: "+name1 +" Score: "+score.timeScore;
-    
+    scoreList1.innerHTML = "Name: " + name1 + " Score: " + score.timeScore;
 }
-/*
-function setScore1() {
-    clearInterval(timeInterval);
-    let scoreList1 = document.createElement("ul")
-    let scoreLi = document.createElement('li')
-    let nameSpan = document.createElement("span")
-    nameSpan.setAttribute("class", "nameSpan")
-    let scoreSpan = document.createElement("span")
-    scoreSpan.setAttribute("class", "scoreSpan")
-    document.getElementById("scoreList").appendChild(scoreList1)
-    scoreList1.appendChild(scoreLi)
-    scoreLi.appendChild(nameSpan)
-    scoreLi.appendChild(scoreSpan)
-    JSON.parse(localStorage.getItem("points"))
-    document.querySelector(".scoreSpan").innerHTML = "Score: "+score.timeScore;
-    let name1 = window.prompt("What is your name?")
-    document.querySelector(".nameSpan").innerHTML = "Name: "+name1
-    localStorage.setItem("name", name1)
-}*/
